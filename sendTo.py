55f43d9aec24760065936cbb4532d65eb9ce03dd
Dario <darobolsogf@gmail.com>
2022-08-23 13:56:49 -0300

    logging debug off

--- sendTo.py
+++ sendTo.py
@@ -16,11 +16,11 @@ def parse_line(payload: str):
     split_payload = payload.split('"')
     payload_end = split_payload[2]
     event_date = payload_end.split('_')[1]
-    print("event_date: {}".format(event_date))
+#    print("event_date: {}".format(event_date))
     message_block = re.findall(r'\[(.*?)\]', payload_end)[0]
     print(message_block)
     res = parse_adc_cid_message(message_block, event_date)
-    print(res)
+#    print(res)
 
     if Config.get('post_send'):
         send_post_sia_event(res)
@@ -36,16 +36,16 @@ def parse_adc_cid_message(message: str, event_date: str) -> dict:
 
     # starts with a #ACCT, so we drop the pound
     account_number = message_blocks[0][1:]
-    print("account_number: {}".format(account_number))
+#    print("account_number: {}".format(account_number))
 
     contact_id = message_blocks[1].split('/')
-    print("contact_id: {}".format(contact_id))
+#    print("contact_id: {}".format(contact_id))
 
     # 1 = New Event or Opening,
     # 3 = New Restore or Closing,
     # 6 = Previously reported condition still present (Status report)
     event_qualifier = contact_id[0][0]
-    print("event_qualifier: {}".format(event_qualifier))
+#    print("event_qualifier: {}".format(event_qualifier))
 
     # 3 decimal(!) digits XYZ (e.g. 602)
     #event_code = contact_id[0][1:]
@@ -53,15 +53,15 @@ def parse_adc_cid_message(message: str, event_date: str) -> dict:
 
     # 2 characters digits XY (e.g. BH)
     event_code = contact_id[1][0:2]
-    print("event_code: {}".format(event_code))
+#    print("event_code: {}".format(event_code))
 
     # 2 decimal(!) digits GG, 00 if no info (e.g. 01)
     group_or_partition_number = contact_id[0]
-    print("group_or_partition_number: {}".format(group_or_partition_number))
+#    print("group_or_partition_number: {}".format(group_or_partition_number))
 
     # 3 decimal(!) digits CCC, 000 if no info (e.g. 001)
     zone_number_or_user_number = int(contact_id[1][2:])
-    print("zone_number_or_user_number: {}".format(zone_number_or_user_number))
+#    print("zone_number_or_user_number: {}".format(zone_number_or_user_number))
 
     return {
         'event_date': event_date,
@@ -74,8 +74,8 @@ def parse_adc_cid_message(message: str, event_date: str) -> dict:
 
 
 def send_post_sia_event(data: dict):
-    print(Config.get('post_send_url'))
-    print("post: {}".format(data))
+#    print(Config.get('post_send_url'))
+#    print("post: {}".format(data))
     send_post_data(Config.get('post_send_url'), data)
 
 
